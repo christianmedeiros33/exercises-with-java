182feeeefa2069c8837d4036cb4c77fdd5f01c72
Christian janke <christianmjanke@gmail.com>
2023-08-09 23:52:49 -0300

    Two exercise finished of methods.

---
+++ src/main/java/exercises/fundamentals/CalculateFactorial.java
@@ -0,0 +1,18 @@
+package exercises.fundamentals;
+
+
+public class CalculateFactorial {
+    public static void main(String[] args) {
+        int number = 22;
+        int numberFactorial = calculatorFactorial(number);
+        System.out.println("The factorial " + number + " is: " + numberFactorial);
+    }
+
+    public static int calculatorFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculatorFactorial(n - 1);
+        }
+    }
+}
---
+++ src/main/java/exercises/fundamentals/NumberIsCousin.java
@@ -0,0 +1,26 @@
+package exercises.fundamentals;
+
+public class NumberIsCousin {
+    public static void main(String[] args) {
+        int number = 23;
+
+        if (isCousin(number)) {
+            System.out.println("is cousin");
+        } else {
+            System.out.println("not cousin");
+        }
+    }
+
+    public static boolean isCousin(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
