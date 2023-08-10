45e9c672e9d5d8db9d6d7ae2e1cc992bdad8e8f3
Christian janke <christianmjanke@gmail.com>
2023-08-09 23:16:50 -0300

    finished exercises of arrays.

---
+++ src/main/java/exercises/fundamentals/BiggerNumber.java
@@ -0,0 +1,11 @@
+package exercises.fundamentals;
+/*
+b) Declare um array de números inteiros e calcule a média dos valores.
+c) Escreva um programa que encontre o valor máximo em um array de números.
+ */
+public class Maximu {
+    public static void main(String[] args) {
+        double measurementCentimeter = 3.5;
+
+    }
+}
